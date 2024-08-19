f3026f74d1eca58d41215561e67ee338210ec291
AkshatSG <akshatsg12@outlook.com>
2024-08-19 14:24:32 +0530

    Initial Commit

---
+++ MpoxAI/MpoxAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MpoxAIApp.swift
+//  MpoxAI
+//
+//  Created by Akshat SG on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct MpoxAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
